78d520934e9e1d1a6718bccbed59df4b66c35c4a
RiyaRoy06 <163673366+RiyaRoy06@users.noreply.github.com>
2024-03-22 00:12:37 +0600

    Add files via upload
    Added week1 day7 problem6 code

--- Week 1/Day 7/Problem 6.cpp
+++ Week 1/Day 7/Problem 6.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
 
+       vector<long long>v;
+       queue<long long>q;
+       int i=0,j=0;
+       while(j<N){
+           if(A[j]<0) q.push(A[j]);
+           if((j-i+1)<K){
+               j++;
+           }
+           else{
+               if(!q.empty()){
+                   v.push_back(q.front());
+                   if(A[i]<0) q.pop();
+
+               }
+               else v.push_back(0);
+               i++;
+               j++;
+           }
+       }
+       return v;
+ }
+int main()
+{
+
+}
