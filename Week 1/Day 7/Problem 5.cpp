2289c9f8f83808061a0b7ba58a5891b30913a7f0
RiyaRoy06 <163673366+RiyaRoy06@users.noreply.github.com>
2024-03-22 00:11:00 +0600

    Add files via upload
    Addede week1 day7 problem5 code

--- Week 1/Day 7/Problem 5.cpp
+++ Week 1/Day 7/Problem 5.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+class Solution{
+public:
+int search(string pat, string txt) {
+    int s = 0;
+    map<char, int> mp;
+    for(auto x : pat)
+        mp[x]++;
+    int c = mp.size();
+    int p = pat.size();
+    int i = 0, j = 0;
+    while(j < txt.length()){
+        if(mp.find(txt[j]) != mp.end()){
+            mp[txt[j]]--;
+            if(mp[txt[j]] == 0)
+                c--;
+        }
+        if(j-i+1<p) j++;
+        else if(j-i+1==p){
+            if(c==0)
+                s++;
+                if(mp.find(txt[i]) != mp.end()){
+                    mp[txt[i]]++;
+                    if(mp[txt[i]] == 1)
+                        c++;
+                }
+                i++;
+                j++;
+        }
+    }
+    return s;
+  }
+};
+int main()
+{
 
+}
