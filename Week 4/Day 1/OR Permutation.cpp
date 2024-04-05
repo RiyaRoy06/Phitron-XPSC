a7705e42cc527d00fb1de669a1ccecd1a3781dea
RiyaRoy06 <163673366+RiyaRoy06@users.noreply.github.com>
2024-04-05 23:10:10 +0600

    Add files via upload
    Added week4 day1 codes

---
+++ Week 4/Day 1/OR Permutation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    for(int i=n;i>=1;i--)
+	    {
+	        cout<<i<<" ";
+	    }
+	    cout<<endl;
+	}
+}
+
---
+++ Week 4/Day 1/Xor wice.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    ll n,m;
+	    cin>>n>>m;
+	    ll p=n^m;
+	    cout<<p<<"\n";
+	}
+
+}
