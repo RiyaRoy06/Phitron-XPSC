0bcb24446ebb8248e85b2646febc8318c0584355
RiyaRoy06 <163673366+RiyaRoy06@users.noreply.github.com>
2024-04-06 23:37:26 +0600

    Add files via upload
    Added week4 day2 codes

---
+++ Week 4/Day 2/Min or Sum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    ll n,p=0,q,m;
+	    cin>>n;
+	    ll a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	        p=p|a[i];
+	    }
+	    q=p;
+	    for(ll i=0;i<n;i++){
+             q=min(q,(p|a[i]));
+	    }
+	    cout<<q<<"\n";
+	}
+}
+
+
+
