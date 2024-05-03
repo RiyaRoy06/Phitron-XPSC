c90cfa09d4dd64485b452a61e1bbea1e6f97509d
RiyaRoy06 <163673366+RiyaRoy06@users.noreply.github.com>
2024-05-03 23:50:32 +0600

    Add files via upload
    Added Week7 day1 codes

---
+++ Week 7/Day 1/Divisible Array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    ll n,k,c=0,m=1,p=0,q,r;
+	    cin>>n;
+	    for(ll i=1;i<=n;i++)
+        {
+            cout<<2*i<<" ";
+        }
+        cout<<"\n";
+	}
+}
+
+
+
+
+
+
+
+
+
+
