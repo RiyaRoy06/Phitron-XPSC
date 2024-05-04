7e35f433b158318d4809bb703f4742ebdca488e3
RiyaRoy06 <163673366+RiyaRoy06@users.noreply.github.com>
2024-05-04 23:48:41 +0600

    Add files via upload
    Added week7 day2 code

---
+++ Week 7/Day 2/3 palindrome.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	ll n;
+	cin>>n;
+	for(ll i=1;i<=n;i++)
+    {
+        if(i%4==1) cout<<"a";
+        else if(i%4==2) cout<<"a";
+        else if(i%4==3)cout<<"b";
+        else cout<<"b";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
